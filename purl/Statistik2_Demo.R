17ba92572b49bc7183efa9b32e88b6a61e509ddf
Nils Ratnaweera <rata@zhaw.ch>
2022-09-19 09:13:10 +0000

    Built site for gh-pages

---
+++ purl/Statistik2_Demo.R
@@ -0,0 +1,160 @@
+a <- c(20, 19, 25, 10, 8, 15, 13 ,18, 11, 14)
+b <- c(12, 15, 16, 7, 8, 10, 12, 11, 13, 10)
+
+blume <- data.frame(cultivar = c(rep("a", 10), rep("b" , 10)), size = c(a, b))
+
+par(mfrow=c(1,1))
+boxplot(size~cultivar, xlab = "Sorte", ylab = "Bluetengroesse [cm]", data = blume)
+
+t.test(size~cultivar, blume, var.equal = T)
+
+aov(size~cultivar, data = blume)
+summary(aov(size~cultivar, data = blume))
+summary.lm(aov(size~cultivar, data = blume))
+
+c <- c(30, 19, 31, 23, 18, 25, 26, 24, 17, 20)
+
+blume2 <- data.frame(cultivar = c(rep("a", 10), rep("b", 10), rep("c", 10)), size = c(a, b, c))
+blume2$cultivar <- as.factor(blume2$cultivar)
+
+summary(blume2)             
+head(blume2)
+
+par(mfrow=c(1,1))
+boxplot(size~cultivar, xlab = "Sorte", ylab = "Blütengrösse [cm]", data = blume2)
+
+aov(size~cultivar, data = blume2)
+summary(aov(size~cultivar, data = blume2))
+summary.lm(aov(size~cultivar, data=blume2))
+
+aov.1 <- aov(size~cultivar, data = blume2)
+summary(aov.1)
+summary.lm(aov.1)
+
+#Berechnung Mittelwerte usw. zur Charakterisierung der Gruppen
+aggregate(size~cultivar, blume2, function(x) c(Mean = mean(x), SD = sd(x), Min = min(x), Max = max(x)))
+
+lm.1 <- lm(size~cultivar, data = blume2)
+summary(lm.1)
+
+#Load library
+if(!require(agricolae)){install.packages("agricolae")}
+library(agricolae)
+
+HSD.test(aov.1, "cultivar", group = FALSE, console = T)
+
+aov.2 <- aov(Sepal.Width ~ Species, data = iris)
+HSD.test(aov.2, "Species", console = T)
+boxplot(Sepal.Width ~ Species, data = iris)
+boxplot(Sepal.Width ~ Species, ylim = c(2, 5), data = iris)
+text(1, 4.8, "a")
+text(2, 4.8, "c")
+text(3, 4.8, "b")
+
+#Load library
+library(tidyverse)
+
+ggplot(iris, aes(Species, Sepal.Width)) + geom_boxplot(size = 1) +
+  annotate("text", y = 5, x = 1:3, label = c("a", "c", "b"))
+
+shapiro.test(blume2$size[blume2$cultivar == "a"])
+var.test(blume2$size[blume2$cultivar == "a"], blume2$size[blume2$cultivar == "b"])
+
+#Load library
+if(!require(car)){install.packages("car")}
+library(car)
+leveneTest(blume2$size[blume2$cultivar == "a"], blume2$size[blume2$cultivar == "b"], center=mean)
+
+wilcox.test(blume2$size[blume2$cultivar == "a"], blume2$size[blume2$cultivar == "b"])
+
+summary(aov(size~cultivar, data = blume2))
+
+kruskal.test(size~cultivar, data = blume2)
+
+#Load library
+if(!require(FSA)){install.packages("FSA")} 
+library(FSA)
+
+#korrigierte p-Werte nach Bejamini-Hochberg
+dunnTest(size~cultivar, method = "bh", data = blume2) 
+
+oneway.test(size~cultivar, var.equal = F, data = blume2)
+
+d <- c(10, 12, 11, 13, 10, 25, 12, 30, 26, 13)
+e <- c(15, 13, 18, 11, 14, 25, 39, 38, 28, 24)
+f <- c(10, 12, 11, 13, 10, 9, 2, 4, 7, 13)
+
+blume3 <- data.frame(cultivar=c(rep("a", 20), rep("b", 20), rep("c", 20)),
+                   house = c(rep(c(rep("yes", 10), rep("no", 10)), 3)),
+                  size = c(a, b, c, d, e, f))
+blume3
+
+boxplot(size~cultivar + house, data = blume3)
+
+summary(aov(size~cultivar + house, data = blume3))
+summary(aov(size~cultivar + house + cultivar:house, data = blume3)) 
+#Kurzschreibweise: "*" bedeutet, dass Interaktion zwischen cultivar und house eingeschlossen wird
+summary(aov(size~cultivar * house, data = blume3)) 
+
+summary.lm(aov(size~cultivar+house, data = blume3))
+
+interaction.plot(blume3$cultivar, blume3$house, blume3$size)
+interaction.plot(blume3$house, blume3$cultivar, blume3$size)
+
+anova(lm(blume3$size~blume3$cultivar*blume3$house), lm(blume3$size~blume3$cultivar+blume3$house))
+anova(lm(blume3$size~blume3$house), lm(blume3$size~blume3$cultivar * blume3$house))
+
+#Load library
+library(car)
+
+blume <- data.frame(a, b)
+scatterplot(a~b, blume)
+
+cor.test(a, b, method = "pearson", data = blume)
+cor.test(a, b, method = "spearman", data = blume)
+cor.test(a, b, method = "kendall", data = blume) 
+
+#Jetzt als Regression
+lm.2 <- lm(b~a)
+anova(lm.2)
+summary(lm.2)
+
+#Model II-Regression
+
+#Load library
+if(!require(lmodel2)){install.packages("lmodel2")} 
+library(lmodel2)
+
+lmodel2(b~a)
+
+par(mfrow=c(2, 2)) #4 Plots in einem Fenster
+plot(lm(b~a))
+
+#Load library
+if(!require(ggfortify)){install.packages("ggfortify")}
+library(ggfortify)
+
+autoplot(lm(b~a))
+
+# Modellstatistik nicht OK
+g <- c(20, 19, 25, 10, 8, 15, 13, 18, 11, 14, 25, 39, 38, 28, 24)
+h <- c(12, 15, 10, 7, 8, 10, 12, 11, 13, 10, 25, 12, 30, 26, 13)
+par(mfrow = c(1, 1))
+
+plot(h~g,xlim = c(0, 40), ylim = c(0, 30))
+abline(lm(h~g))
+
+par(mfrow = c(2, 2))
+plot(lm(h~g))
+
+# Modelldiagnostik mit ggplot
+df <- data.frame(g, h)
+ggplot(df, aes(x = g, y = h)) + 
+    # scale_x_continuous(limits = c(0,25)) +
+    # scale_y_continuous(limits = c(0,25)) +
+    geom_point() +
+    geom_smooth( method = "lm", color = "black", size = .5, se = F) + 
+    theme_classic()
+
+par(mfrow=c(2, 2))
+autoplot(lm(h~g))
---
+++ purl/Statistik3_Demo.R
@@ -0,0 +1,137 @@
+compensation <- read.delim("data/ipomopsis.csv", sep = ",", stringsAsFactors = T)
+
+summary(compensation)
+
+plot(Fruit~Root, data = compensation)
+boxplot(Fruit~Grazing, data = compensation)
+
+tapply(compensation$Fruit, compensation$Grazing, mean)
+
+aoc.1 <- lm(Fruit~Root * Grazing, data = compensation)
+summary.aov(aoc.1)
+
+aoc.2 <- lm(Fruit~Grazing * Root, data = compensation)
+summary.aov(aoc.2)
+
+aoc.3 <- lm(Fruit~Grazing + Root, data = compensation)
+summary.lm(aoc.3)
+
+# Plotten der Ergebnisse
+library(tidyverse)
+ggplot(compensation, aes(Root, Fruit, color = Grazing)) +
+  geom_point() + theme_classic()
+
+# Ploten mit base R
+plot(Fruit~Root, pch = 16, col = Grazing, data = compensation)
+legend("topleft", c("grazed", "ungrazed"), col = c("black","red"), pch = 16) 
+
+e <- c(20, 19, 25, 10, 8, 15, 13, 18, 11, 14, 25, 39, 38, 28, 24)
+f <- c(12, 15, 10, 7, 2, 10, 12, 11, 13, 10, 9, 2, 4, 7, 13)
+
+lm.1 <- lm(f~e)
+lm.quad <- lm(f~e + I(e^2))
+
+summary(lm.1)
+summary(lm.quad)
+
+par(mfrow = c(1, 1))
+
+# 1. lineares Modell
+plot(f~e, xlim = c(0, 40), ylim = c(0, 20))
+abline(lm(f~e), col = "blue")
+
+# 2. quadratisches Modell
+xv <- seq(0, 40, 0.1)
+plot(f~e, xlim = c(0, 40), ylim = c(0, 20))
+yv2 <- predict(lm.quad, list(e = xv))
+lines(xv, yv2, col = "red")
+
+# Residualplots
+par(mfrow = c(2, 2))
+plot(lm.1)
+plot(lm.quad)
+
+test <- data.frame("x" = c(1, 2, 3, 4, 5, 6), "y" = c(34, 21, 70, 47, 23, 45))
+
+par(mfrow=c(1,1))
+plot(y~x, data = test)
+
+lm.0 <- lm(y~1, data = test)
+lm.1 <- lm(y~x, data = test)
+lm.2 <- lm(y~x+ I(x^2), data = test)
+lm.3 <- lm(y~x+ I(x^2) + I(x^3), data = test)
+lm.4 <- lm(y~x+ I(x^2) + I(x^3) + I(x^4), data = test)
+lm.5 <- lm(y~x+ I(x^2) + I(x^3) + I(x^4) + I(x^5), data = test)
+lm.6 <- lm(y~x+ I(x^2) + I(x^3) + I(x^4) + I(x^5) + I(x^6), data = test)
+summary(lm.0)
+summary(lm.1)
+summary(lm.2)
+summary(lm.3)
+summary(lm.4)
+summary(lm.5)
+
+xv <- seq(from = 0, to = 10, by = 0.1)
+
+plot(y~x, cex = 2, col = "black", lwd = 3, data = test)
+yv <- predict(lm.1, list(x = xv))
+lines(xv, yv, col = "red", lwd = 3)
+yv <- predict(lm.2, list(x = xv))
+lines(xv, yv, col = "blue", lwd = 3)
+yv<-predict(lm.3, list(x = xv))
+lines(xv, yv, col = "green", lwd =3)
+yv <- predict(lm.4, list(x = xv))
+lines(xv, yv, col = "orange", lwd = 3)
+yv <- predict(lm.5, list(x = xv))
+lines(xv, yv, col = "black", lwd = 3)
+
+loyn <- read.delim("data/loyn.csv", sep = ",")
+summary(loyn)
+
+cor <- cor(loyn[, 2:7])
+print(cor, digits = 2)
+
+cor[abs(cor)<0.6] <- 0
+cor
+print(cor, digits = 3)
+
+lm.1 <- lm (ABUND ~ YR.ISOL + ALT + GRAZE, data = loyn)
+if(!require(car)){install.packages("car")} 
+library(car)
+vif(lm.1)
+
+influence.measures(lm.1)
+
+lm.1 <- lm(ABUND ~ YR.ISOL + ALT + GRAZE, data = loyn)
+summary(lm.1)
+
+lm.2 <- update(lm.1,~.-YR.ISOL)
+anova(lm.1, lm.2)
+summary(lm.2)
+
+lm.3 <- update(lm.2,~.-ALT)
+anova(lm.2, lm.3)
+summary(lm.3)
+
+par(mfrow = c(2, 2))
+plot(lm.1)
+
+if(!require(hier.part)){install.packages("hier.part")}
+library(hier.part)
+
+loyn.preds <-with(loyn, data.frame(YR.ISOL, ALT, GRAZE))
+hier.part(loyn$ABUND, loyn.preds, gof = "Rsqu")
+
+avPlots(lm.1, ask = F)
+
+if(!require(MuMIn)){install.packages("MuMIn")}
+library(MuMIn)
+
+global.model <- lm(ABUND ~ YR.ISOL + ALT + GRAZE, data = loyn)
+options(na.action = "na.fail")
+
+allmodels <- dredge(global.model)
+allmodels
+sw(allmodels)
+
+avgmodel <- model.avg(allmodels, subset = TRUE)
+summary(avgmodel)
